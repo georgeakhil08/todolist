1ac476449a31821494ff423c8aa56f52e072f61d
Akhil George <you@example.comgeorgeakhil8920@gmail.com>
2023-11-22 23:17:34 +0530

    first commit

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,12 +5,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor(private http: HttpClient) { }
-
-  getTodo() {
-    return this.http.get("https://jsonplaceholder.typicode.com/todos/")
+  create(post: { title: string; }) {
+    throw new Error('Method not implemented.');
   }
+
+  constructor(private http:HttpClient) { }
+
+
+
+
+getTodo()
+{
+  return this.http.get("https://jsonplaceholder.typicode.com/todos/")
+}
+deleteTodo(id:any)
+{ 
+  
+    return this.http.delete("https://jsonplaceholder.typicode.com/todos/"+id)
+
+}
+creates(post:any)
+{
+  return this.http.post("https://jsonplaceholder.typicode.com/todos/", JSON.stringify(post));
 }
 
-
+}
